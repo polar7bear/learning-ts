a097e8241241c45dcb0b644e5a5b612155ab9bc2
polar7bear <sons1998@naver.com>
2024-07-15 15:24:06 +0900

    call-signature, index-signatrue

---
+++ 05.call-signature, index-signature/src/main.ts
@@ -0,0 +1,36 @@
+// call signature
+interface getLikeNumber {
+    (like: number): number;
+}
+
+interface Post {
+    id: number;
+    title: string;
+    getLikeNumber: getLikeNumber;
+}
+const post1: Post = {
+    id: 1,
+    title: 'post 1',
+    getLikeNumber(like: number) {
+        return like;
+    }
+}
+
+post1.getLikeNumber(1);
+
+
+// index signatrue
+
+interface Post2 {
+    [key: string]: unknown;
+    id: number;
+    title: string;
+}
+
+const post2: Post2 = {
+    id: 1,
+    title: 'post 2'
+}
+
+post2['description'] = 'description 2';
+post2['pages'] = 225;
